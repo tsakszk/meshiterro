48e643517ed26f7a8cce37bd43eb5e9cb7ddb979
vagrant <vagrant@localhost.localdomain>
2020-03-31 09:30:49 +0000

    [finish]5-1~7

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'post_images#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resource :post_comments, only: [:create, :destroy]
+end
+  resources :users, only: [:show, :edit, :update]
 end
